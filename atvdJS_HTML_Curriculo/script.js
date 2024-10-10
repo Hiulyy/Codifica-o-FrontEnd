9dca4fe3708b742b0c56806030aed26792d73d68
Hiulyy <hiuly.github@gmail.com>
2024-10-10 12:43:22 -0300

    Add files via upload

---
+++ atvdJS_HTML_Curriculo/script.js
@@ -0,0 +1,26 @@
+const titulo = document.querySelector(".titulo");
+titulo.textContent = "Hiuly Barbosa Silva"
+titulo.style.textAlign = "Center"
+
+const info = document.getElementById("info");
+info.textContent = "Brasileira,Solteira,16 anos,"
+info.style.textAlign =  "Center"
+
+const email = document.getElementById("email");
+email.textContent = "Email: hiulysilva5@gmail.com"
+email.style.textAlign =  "Center"
+email.style.marginTop = "-10px"
+
+const contato = document.getElementById("contato");
+contato.textContent = "Contato: (82)99368-5223"
+contato.style.textAlign =  "Center"
+contato.style.marginTop = "-10px"
+
+const end = document.getElementById("end");
+end.textContent = "Av. Menino Marcelo, 1391 - Cidade Universitária, Maceió - AL, 57073-460"
+end.style.textAlign =  "Center"
+end.style.marginTop = "-10px"
+
+const objetivo = document.querySelector(".objetivo");
+objetivo.textContent = "Objetivo:"
+objetivo.style.textAlign = "Center"
