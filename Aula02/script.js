be03bca75f48d97cde26f1406760afdbf3ed3a02
Hiulyy <hiuly.github@gmail.com>
2025-02-05 12:35:38 -0300

    Add files via upload

---
+++ Aula02/script.js
@@ -0,0 +1,11 @@
+document.getElementById("adicionarTarefa").addEventListener("click",
+    function (){
+
+        const inputTarefa = document.querySelector("#novaTarefa");
+        const tarefaTexto = inputTarefa.value;
+        
+        if(tarefaTexto === ""){
+            alert("O campo não pode estar vazio!")
+        }
+    });
+    
